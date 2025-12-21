cbbc6c4ff93d8bac049292c99457e15baadad8c2
ghkhabaz <161433179+ghkhabaz@users.noreply.github.com>
2025-12-21 09:21:52 +0330

    Update schoolapp.py

--- schoolapp.py
+++ schoolapp.py
@@ -263,4 +263,5 @@ def demo_report():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
